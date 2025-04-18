8e657327e82e75a45bbc6f09f5c9b6c6130a00a9
xavier jouppe <xajouppe@gmail.com>
2025-04-18 16:15:56 +0200

    feat: wrap queries in transactions

--- src/repositories/users.py
+++ src/repositories/users.py
@@ -7,15 +7,18 @@ class UsersRepository:
         self.db_session = db_session
 
     async def get_user_by_email(self, email: str) -> UserBase:
-        query = "SELECT * FROM d_roles.users WHERE email = :email"
-        return await self.db_session.fetch_one(query, {"email": email})
+        async with self.db_session.transaction():
+            query = "SELECT * FROM d_roles.users WHERE email = :email"
+            return await self.db_session.fetch_one(query, {"email": email})
 
     async def add_user(self, user: UserCreate) -> int:
-        query = "INSERT INTO d_roles.users (email, is_email_confirmed) VALUES (:email, :is_email_confirmed) RETURNING id"
-        values = {"email": user.email, "is_email_confirmed": False}
-        new_user_id = await self.db_session.execute(query, values)
-        return new_user_id
+        async with self.db_session.transaction():
+            query = "INSERT INTO d_roles.users (email, is_email_confirmed) VALUES (:email, :is_email_confirmed) RETURNING id"
+            values = {"email": user.email, "is_email_confirmed": False}
+            new_user_id = await self.db_session.execute(query, values)
+            return new_user_id
 
     async def get_user_by_id(self, user_id: int) -> UserBase:
-        query = "SELECT * FROM d_roles.users WHERE id = :id"
-        return await self.db_session.fetch_one(query, {"id": user_id})
+        async with self.db_session.transaction():
+            query = "SELECT * FROM d_roles.users WHERE id = :id"
+            return await self.db_session.fetch_one(query, {"id": user_id})

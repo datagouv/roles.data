3bebd08e90b00bd69f9e848d1f9a8ac08d442087
xavier jouppe <xajouppe@gmail.com>
2025-07-09 12:08:36 +0200

    fix: use relative imports

--- src/routers/web/admin/view.py
+++ src/routers/web/admin/view.py
@@ -1,10 +1,9 @@
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 
-from routers.web.admin.pages import service_providers_view
 from templates.template_manager import template_manager
 
-from .pages import groups_view, logs_view
+from .pages import groups_view, logs_view, service_providers_view
 
 router = APIRouter(
     prefix="/admin",

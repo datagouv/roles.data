99d6d2c55f4de41ce372f0b303fe0379d80e25b9
xavier jouppe <xajouppe@gmail.com>
2025-08-13 14:25:43 +0200

    fix: random case email

---
+++ db/migrations/20250813.sql
@@ -0,0 +1,13 @@
+\set schema_name :DB_SCHEMA
+
+BEGIN;
+
+-- Migration: Convert all emails to lowercase
+-- Date: 2025-08-13
+
+-- Update all emails in users table to lowercase
+UPDATE :schema_name.users
+SET email = LOWER(email)
+WHERE email != LOWER(email);
+
+COMMIT;

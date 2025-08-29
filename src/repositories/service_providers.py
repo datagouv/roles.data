b1a418674414e1d4ad35a09568e21e5bb58d15f1
xavier jouppe <xajouppe@gmail.com>
2025-08-29 12:44:01 +0200

    feat: add a link to the service provider after activation

--- src/repositories/service_providers.py
+++ src/repositories/service_providers.py
@@ -16,3 +16,17 @@ class ServiceProvidersRepository:
             return await self.db_session.fetch_one(
                 query, {"service_provider_id": service_provider_id}
             )
+
+    async def find_by_user(self, user_email: str) -> list[ServiceProviderResponse]:
+        async with self.db_session.transaction():
+            query = """
+            SELECT DISTINCT SP.id, SP.name, SP.url
+            FROM service_providers as SP
+            JOIN group_service_provider_relations as GSPR ON SP.id = GSPR.service_provider_id
+            JOIN groups as G ON GSPR.group_id = G.id
+            JOIN group_user_relations as GUR ON G.id = GUR.group_id
+            JOIN users as U ON GUR.user_id = U.id
+            WHERE U.email = :user_email
+            ORDER BY SP.id
+            """
+            return await self.db_session.fetch_all(query, {"user_email": user_email})
--- src/services/ui/activation_service.py
+++ src/services/ui/activation_service.py
@@ -1,19 +1,28 @@
 from pydantic import UUID4
 
 from ...model import UserResponse
+from ...repositories.service_providers import ServiceProvidersRepository
 from ...repositories.users import UsersRepository
 
 
 class ActivationService:
-    def __init__(self, user_repository: UsersRepository):
-        self.user_repository = user_repository
+    def __init__(
+        self,
+        users_repository: UsersRepository,
+        service_providers_repository: ServiceProvidersRepository,
+    ):
+        self.users_repository = users_repository
+        self.service_providers_repository = service_providers_repository
 
     async def activate_user(self, user_email: str, user_sub: UUID4) -> UserResponse:
-        user = await self.user_repository.get_by_email(email=user_email)
+        user = await self.users_repository.get_by_email(email=user_email)
 
         if not user.is_verified:
-            return await self.user_repository.activate(
+            return await self.users_repository.activate(
                 user_sub=user_sub, user_email=user_email
             )
 
         return user
+
+    async def get_user_providers(self, user_email: str):
+        return await self.service_providers_repository.find_by_user(user_email)

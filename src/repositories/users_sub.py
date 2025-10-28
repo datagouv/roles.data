6534402d674f6bb8a99f444818ce4343cd72d835
Xavier Jp <xavier.jouppe@beta.gouv.fr>
2025-10-28 13:20:53 +0100

    Update src/repositories/users_sub.py
    Co-authored-by: Hajar AEK <48837850+HAEKADI@users.noreply.github.com>

--- src/repositories/users_sub.py
+++ src/repositories/users_sub.py
@@ -6,7 +6,7 @@ from pydantic import EmailStr
 
 class UserSubsRepository:
     """
-    This repository manipulates the user sub (set/get)
+    This repository handles the user sub (set/get)
 
     It is separated from UsersRepository, that does not handle the sub directly for security purpose
     """
